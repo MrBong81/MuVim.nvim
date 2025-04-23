89d18960a7b8adbc3740e36cee4e300028eeba1b
MrBong <thepowerofthebong@gmail.com>
2025-04-23 17:59:15 +0200

    Customised R-nvim appearance and some basic functionalities

--- lua/custom/plugins/R-nvim.lua
+++ lua/custom/plugins/R-nvim.lua
@@ -4,5 +4,36 @@ return {
     -- Only required if you also set defaults.lazy = true
     lazy = false,
     version = '~0.9.21',
+    config = function()
+      -- Create a table with the options to be passed to setup()
+      ---@type RConfigUserOpts
+      local opts = {
+        -- R_args = {"--quiet", "--no-save"},
+
+        -- make sure that the console is on the right
+        rconsole_width = 57,
+        min_editor_width = 18,
+
+        -- make sure the object browser appears bottom of the script
+        objbr_place = 'script,below',
+
+        -- enable autoquit of r when quitting nvim
+        auto_quit = true,
+        objbr_mappings = { -- Object browser keymap
+          c = 'class', -- Call R functions
+          ['<localleader>gg'] = 'head({object}, n = 15)', -- Use {object} notation to write arbitrary R code.
+          v = function()
+            require('r.browser').toggle_view()
+          end,
+        },
+        disable_cmds = {
+          'RClearConsole',
+          'RCustomStart',
+          'RSPlot',
+          'RSaveClose',
+        },
+      }
+      require('r').setup(opts)
+    end,
   },
 }
