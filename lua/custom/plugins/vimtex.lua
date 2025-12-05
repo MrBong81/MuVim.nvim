1c8a8fb116e0a331e1d43a083410ffbf73dc7177
MrBong <thepowerofthebong@gmail.com>
2025-12-05 22:51:35 +0100

    Removed bug with citations, due to the .latexmk directory: do not copy configs from random internet people

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -4,10 +4,10 @@ return {
   config = function()
     vim.g.vimtex_view_method = 'sioyek'
     vim.g.vimtex_view_sioyek_exe = 'C:/Users/Ermenegisto/.sioyek/sioyek-release-windows/sioyek.exe'
-    vim.g.vimtex_compiler_latexmk = {
-      aux_dir = './.latexmk/aux',
-      out_dir = './.latexmk/out',
-    }
+    -- vim.g.vimtex_compiler_latexmk = {
+    --   aux_dir = './.latexmk/aux',
+    --   out_dir = './.latexmk/out',
+    -- }
   end,
   keys = {
     { '<localLeader>l', '', desc = '+vimtex' },
